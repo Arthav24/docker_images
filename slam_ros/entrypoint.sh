981d00442840951a5f454bb333930b40d269cf65
Anirudh Swarankar <swarnakar.ani24@gmail.com>
2021-05-16 21:29:56 +0530

    "adding USER ros in container"

--- slam_ros/entrypoint.sh
+++ slam_ros/entrypoint.sh
@@ -5,21 +5,21 @@ source /opt/ros/melodic/setup.bash
 echo "ROS Sourced"
 
 #Checking if source exists or not
-if [ ! -d /slam_ws/src ]
+if [ ! -d /home/ros/slam_ws/src ]
 then
-mkdir -p /slam_ws/src 
-cd /slam_ws/src
+mkdir -p /home/ros/slam_ws/src 
+cd /home/ros/slam_ws/src
 git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git -b melodic-devel
 git clone  https://github.com/ROBOTIS-GIT/turtlebot3.git -b melodic-devel
-cd /slam_ws
+cd /home/ros/slam_ws
 catkin build
-cd /slam_ws/src
+cd /home/ros/slam_ws/src
 git clone https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git
 catkin build 
 fi
-if [ -f /slam_ws/devel/setup.bash ]
+if [ -f /home/ros/slam_ws/devel/setup.bash ]
 then
-source /slam_ws/devel/setup.bash
+source /home/ros/slam_ws/devel/setup.bash
 echo "workspace sourced"
 fi
 export TURTLEBOT3_MODEL=waffle
